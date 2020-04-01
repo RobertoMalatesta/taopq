a43189d5da4136ef743c60135c94618b23086343
Daniel Frey <d.frey@gmx.de>
2020-04-01 22:56:58 +0200

    Remove obsolete

--- src/lib/pq/connection_pool.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016-2020 Daniel Frey and Dr. Colin Hirsch
-// Please see LICENSE for license or visit https://github.com/taocpp/taopq/
-
-#include <tao/pq/connection_pool.hpp>
-
-namespace tao::pq
-{
-}  // namespace tao::pq
